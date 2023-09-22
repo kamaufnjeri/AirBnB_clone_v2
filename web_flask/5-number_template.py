f6a199d486447bcefbf107834ea9dd0bc8067f70
kamaufnjeri <kamaufnjeri2019@gmail.com>
2023-09-22 17:53:17 +0300

    odd or even on html page

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def number(n):
     return "{} is a number".format(n)
 
 
-@app.route("/number_template/<int:n>", strict_slashes=Fals)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Display a html page and also display a number"""
     return render_template("5-number.html", n=n)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,7 @@ def number_template(n):
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def odd_or_even(n):
+def number_odd_or_even(n):
     """Display number and if its odd or even"""
     return render_template("6-number_odd_or_even.html", n=n)
 
